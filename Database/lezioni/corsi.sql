a160a56f2362d7c25ad400c96d61b8830a163c48
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2024-12-17 12:55:20 +0100

    Create corsi.sql

---
+++ Database/lezioni/corsi.sql
@@ -0,0 +1,36 @@
+
+
+INSERT INTO corsi (nome) VALUES 
+('Materiale condiviso - Cartella del partecipante'),
+('Materiale condiviso - Docenti'),
+('Approfondimenti didattici e Orientamento'),
+('Architetture e infrastrutture Software'),
+('Base di dati - NoSQL'),
+('Base di dati - SQL'),
+('Cloud services'),
+('Containers - Docker'),
+('Copyright e norme giuridiche del mondo digitale'),
+('Fondamenti di basi di dati'),
+('Fondamenti di programmazione'),
+('Fondamenti di reti di calcolatori'),
+('Fondamenti di UX/UI e HTML/CSS'),
+('Fondamenti di Version Control'),
+('Inglese'),
+('Laboratorio di preparazione project work'),
+('Learning by Project - Soft Skill'),
+('Learning by Project - Tech'),
+('Linux Server'),
+('Orientamento al lavoro'),
+('Parità fra uomini e donne e non discriminazione'),
+('Percorso di sviluppo soft skills'),
+('Programmazione - .NET Framework / C#'),
+('Programmazione - Java'),
+('Programmazione - PHP'),
+('Programmazione WEB - Javascript'),
+('Sicurezza informatica'),
+('Sicurezza sul lavoro'),
+('Strumenti AI e Prompt Engineerin');
+
+SELECT * FROM corsi;
+
+DELETE FROM corsi WHERE id = 1;
