5498260ab1c77da6facca9fd510443cef180427d
maboglia <mauro.bogliaccino@gmail.com>
2025-01-21 12:09:26 +0100

    aggiornamento codice sviluppato in FAD

--- Database/lezioni/lezione_2025-01-21.sql
+++ Database/lezioni/lezione_2025-01-21.sql
@@ -7,8 +7,72 @@ CREATE TABLE `studenti_swd` (
   `cognome` varchar(40) DEFAULT NULL
 );
 
-select * from studenti_swd;
-
 ALTER TABLE `fondamenti`.`studenti_swd` 
 ADD COLUMN `data_nascita` DATE NULL AFTER `cognome`;
+
+ALTER TABLE studenti_swd
+AUTO_INCREMENT = 1;
+
+-- -------------------------------------------------------
+
+-- DML
+-- CRUD; Create Read Update Delete
+
+-- create
+insert into studenti_swd 
+values 
+	(1, 'nome', 'cognome', '2005-01-21'),
+	(2, 'nome', 'cognome', '2004-01-21');
+
+insert into studenti_swd (nome, cognome, data_nascita)
+values 
+	('nome', 'cognome', '2005-01-21'),
+	('nome', 'cognome', '2004-01-21');
+
+-- read
+select * from studenti_swd;
+
+-- update
+update studenti_swd set nome = 'pippo' where id = 3;
+
+-- delete
+delete from studenti_swd  where id > 0;
+truncate studenti_swd;
+
+INSERT INTO studenti_swd (nome, cognome, data_nascita)
+VALUES
+('nesma','abd el aziz','2000-08-14'),
+('Andrea','Alume','2004-05-11'),
+('Andrea','Bellissimo','2005-11-05'),
+('Nicolas','Bellotti','1996-02-13'),
+('Omar','Benagoub','2005-07-01'),
+('Maria','Cernat','1995-08-31'),
+('Xiaolong','Chen','2004-08-08'),
+('Simone','Chimirri','2004-02-01'),
+('Simone','Crivello','1996-07-19'),
+('Alessandro','De Filippis','2002-06-04'),
+('Luca','Di Pasquale','2004-05-13'),
+('Marco','Dima','2002-05-15'),
+('Alessio','Divizia','2002-06-10'),
+('Tommaso','Ferrero','2003-07-06'),
+('Alessandro','Formicola','2004-08-16'),
+('Alessandro','Grotta','2004-05-08'),
+('Jiacheng','Hu','1999-05-17'),
+('Dario','Ilescu','2005-08-15'),
+('Angelo','Jimenez Moscoso','1996-01-22'),
+('Emanuele','Macis','2002-07-06'),
+('Lorenzo','Maero','2004-08-05'),
+('Yaroslav','Nechainikov','2005-03-11'),
+('denis angelo','oniga','2004-10-05'),
+('Jacopo','Parlacino','2005-06-08'),
+('Simone','Pizzorno','2005-08-09'),
+('Daniela','Punzi','2005-03-15'),
+('Andrea','Sabini','1999-02-28'),
+('marco','spedaliere','2005-07-24'),
+('Ulrich','Tchiengoua ','1997-06-21'),
+('domenico','vardè','2004-06-21');
+
+
+
+
 
